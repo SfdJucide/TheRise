664e7623a759d8207921b471d39ac4110d235f01
sfd <kater167761@gmail.com>
2022-07-08 12:37:46 +0300

    add form for task

--- theRise/tasks/admin.py
+++ theRise/tasks/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from tasks.models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rank', 'status')
+    list_display_links = ('name',)
+    search_fields = ('name',)
--- theRise/tasks/views.py
+++ theRise/tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from tasks.models import Task
+from tasks.forms import TaskForm
 
 def tasks_list(request):
     context = {
         'title': 'Список задач',
-        'tasks': Task.objects.all()
+        'tasks': Task.objects.all(),
+        'form': TaskForm
     }
     return render(request, 'task_list.html', context)
