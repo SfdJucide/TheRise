69a626ad4085d33687e41774f3f10b6282f012aa
sfd <kater167761@gmail.com>
2022-07-18 23:56:33 +0300

    add model carcass for users

--- theRise/users/models.py
+++ theRise/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='фото')
+    age = models.PositiveIntegerField(null=True, verbose_name='возраст')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=False, unique=True, on_delete=models.CASCADE, db_index=True)
+    
